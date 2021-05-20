5286c2a3d8135934619ea8fee376db9cbea50bc9
guangyongluo(罗葳) <llooww@mail.ustc.edu.cn>
2021-05-20 11:29:22 +0800

    mybatis-plus(initial repository) : use BaseMapper implements CRUD process.

---
+++ src/main/java/com/vilin/mybatisplus/entity/User.java
@@ -0,0 +1,11 @@
+package com.vilin.mybatisplus.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
---
+++ src/main/java/com/vilin/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.vilin.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.vilin.mybatisplus.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
